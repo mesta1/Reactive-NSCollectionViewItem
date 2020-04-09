fa69f572be3cf5be4f589b95277a3991934bfb64
Michele Cattafesta <michele.cattafesta@synchronoss.com>
2020-04-09 09:51:51 +0100

    Reactive CollectionViewItem sample

---
+++ ReactiveCollectionView/ItemViewController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ReactiveUI;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Disposables;
+using Foundation;
+using AppKit;
+
+namespace ReactiveCollectionView
+{
+    // The xib MUST have the same name of this class (ItemViewController.xib)
+    public partial class ItemViewController : ReactiveCollectionViewItem<ItemViewModel>
+    {
+        private CompositeDisposable _disposables;  
+        
+        public ItemViewController(IntPtr handle) : base(handle)
+        {
+            Initialize();
+        }
+        
+        [Export("initWithCoder:")]
+        public ItemViewController(NSCoder coder) : base(coder)
+        {
+            Initialize();
+        }
+     
+        public ItemViewController() : base("ItemView", NSBundle.MainBundle)
+        {
+            Initialize();
+        }
+       
+        void Initialize()
+        {
+        }       
+
+        //strongly typed view accessor
+        public new ItemView View
+        {
+            get
+            {
+                return (ItemView)base.View;
+            }
+        }
+
+        public void UpdateUI()
+        {
+            _disposables?.Dispose();
+            _disposables = new CompositeDisposable();
+            this.Bind(ViewModel, vm => vm.Checked, v => v._check.State, _check.ObservableActivated(),
+                vmv => vmv ? NSCellStateValue.On : NSCellStateValue.Off, vvm => vvm == NSCellStateValue.On)
+                .DisposeWith(_disposables);
+
+            this.OneWayBind(ViewModel, vm => vm.Name, v => v._check.Title)
+              .DisposeWith(_disposables);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _disposables?.Dispose();
+        }
+    }
+
+    // DO NOT COPY, IS ALREADY THERE
+
+    public static class SignalHelpers
+    {
+        public static IObservable<Unit> ObservableActivated(this NSButton button)
+        {
+            return Observable
+                .FromEventPattern<EventHandler, EventArgs>(
+                    h => button.Activated += h, h => button.Activated -= h)
+                .Select(_ => Unit.Default);
+        }
+
+        public static IObservable<Unit> ObservableTextChanged(this NSTextField textField)
+        {
+            var ret = new Subject<Unit>();
+            textField.Delegate =
+                new BlockDidChangeTextFieldDelegate(() => ret.OnNext(Unit.Default));
+            return ret;
+        }
+
+        private class BlockDidChangeTextFieldDelegate : NSTextFieldDelegate
+        {
+            private readonly Action block;
+
+            public BlockDidChangeTextFieldDelegate(Action block)
+            {
+                this.block = block;
+            }
+
+            public override void Changed(NSNotification notification)
+            {
+                block();
+            }
+        }
+    }
+}
